38bdbd0568984c7a6e80527eda4d02513671a12f
Larry Sheradon <sheradon@gmail.com>
2012-05-12 02:19:04 +0000

    looks better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,12 +12,14 @@ class HomeController < ApplicationController
       :elements => @others,
       :key      => { '#446699' => 'Other Schools' } )
     @map = Bedrock::Map.new({
-      :extent         => Bedrock::city_extents(:detroit),
+      #:extent         => Bedrock::city_extents(:detroit),
       :base_layers    => ['street'],
       :layers         => [ @school_overlay, @other_overlay ],
       :layer_control  => true,
-      :min_zoom => 10,
+      :center => { :lon => -83.09577941894531, :lat => 42.364885996366525 },
+      :min_zoom => 11,
       :max_zoom => 18,
+      :zoom => 12,
     })
   end
 end
