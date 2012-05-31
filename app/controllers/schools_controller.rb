dc9231d3a78f8d571d826d4efe448dd4e9113821
Larry Sheradon <sheradon@gmail.com>
2012-05-31 03:31:27 +0000

    jqplot

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -19,9 +19,18 @@ class SchoolsController < ApplicationController
       :center => { :lon => @school.centroid.x, :lat => @school.centroid.y },
       :min_zoom => 12,
       :max_zoom => 18,
-      :zoom => 15,
+      :zoom => 12,
     })
-    
+
+    # X, S, M, W, R
+    cats = ['X', 'S', 'M', 'W', 'R']
+    school = cats.collect { |i| @school["PCT_PROF_ALLSTUD_11_#{i}_MME_2011"] }
+    school = school.enum_for(:each_with_index).collect { |score, index| [score.to_i, index+1] }
+    state = school.collect { |a| [a[0] + 12*rand, a[1]] }
+    @spring = {
+      :state => state,
+      :school => school
+    }
     
     respond_to do |format|
       format.html { }
