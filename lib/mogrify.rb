b7c42657de0780252a12ba08945da6f40d29587d
Larry Sheradon <sheradon@gmail.com>
2012-07-03 01:08:00 +0000

    file not found!!

---
+++ lib/mogrify.rb
@@ -0,0 +1,26 @@
+module Mogrify
+
+  def transliterate(str)
+    # Escape str by transliterating to UTF-8 with Iconv
+    #s = Iconv.iconv('ascii//ignore//translit', 'utf-8', str).to_s
+    s = str
+  
+    s.downcase!
+    s.gsub!(/'/, '')
+    s.gsub!(/[^A-Za-z0-9]+/, ' ')
+    s.strip!
+    s.gsub!(/\ +/, '-')
+  
+    return s
+  end  
+  
+  private
+  
+  def transliterate_file_name(str)
+    logger.info "tfn gets: #{str}"
+    extension = File.extname(str).gsub(/^\.+/, '')
+    filename = str.gsub(/\.#{extension}$/, '')
+    return "#{transliterate(filename)}.#{transliterate(extension)}"
+  end  
+
+end
