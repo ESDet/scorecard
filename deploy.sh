f14e38925e2e0c0484980685e1d9eb1a4ca72075
L. Sheradon <sheradon@gmail.com>
2013-04-01 18:20:04 -0700

    oh and deploy to use git

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn update
+git pull
 lessc public/stylesheets/style.less > public/stylesheets/style.css
 lessc public/stylesheets/print.less > public/stylesheets/print.css
 touch tmp/restart.txt
