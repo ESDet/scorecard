6904ae9dbe7525457414ea0d4a581283945cc0e7
L. Sheradon <sheradon@gmail.com>
2013-08-02 17:12:36 -0700

    first attempt at json school list updating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,38 +4,7 @@ class HomeController < ApplicationController
   def index
     session[:filter] = session[:loc] = nil
   end
-  
-  
-  def map
-    #@schools = School.all
-    #@districts = District.all
-    
-    @school_o = Bedrock::Overlay.from_config('schools',
-      :ty         => :geojson,
-      :url        => schools_path(:format => :json, :filter => session[:filter], :loc => session[:loc], :type => session[:type]),
-      :mouseover  => true,
-      :key => {
-        '#8DC63F' => 'Elementary Schools',
-        '#F79421' => 'Middle Schools',
-        '#004270' => 'High Schools',
-      })
-    @district_o = Bedrock::Overlay.from_config('districts',
-      :ty => :geojson,
-      :elements => [District.find(580)])
-    @map = Bedrock::Map.new({
-      #:extent         => Bedrock::city_extents(:detroit),
-      :base_layers    => ['street'],
-      :layers         => [ @district_o, @school_o ],
-      :layer_control  => true,
-      :center => { :lon => -83.09577941894531, :lat => 42.364885996366525 },
-      :min_zoom => 11,
-      :max_zoom => 18,
-      :zoom => 12,
-      :layer_control => false,
-    })
 
-    render layout: 'noside'
-  end
   
   def search
     if params[:q]
