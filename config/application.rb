772b87a96a676f7e53a18084cfb500630bdca613
Alec Tower <alectower@gmail.com>
2015-04-13 14:30:39 -0400

    Add buildpacks for geos and proj

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-
 require 'rails/all'
 require 'rgeo/geo_json'
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-Dotenv.load
+if Rails.env == :development || Rails.env == :test
+  require 'dotenv'
+  Dotenv.load
+end
 
 module ESD
   class Application < Rails::Application
