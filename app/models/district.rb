a27f07aa1ef5e1b3c423b8ebd607a70ed3590177
Alec Tower <alectower@gmail.com>
2015-06-09 13:35:13 -0400

    Remove old models and rake task for storing schools

--- app/models/district.rb
+++
@@ -1,5 +0,0 @@
-class District < ActiveRecord::Base
-  set_primary_key 'OGR_FID'
-  require 'bedrock/acts_as_feature'
-  acts_as_feature :geometry => 'SHAPE', :index => 'SHAPE', :fields => [:OGR_FID]
-end
