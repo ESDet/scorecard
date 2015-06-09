ec93f84f201caed4655ba1af19fed6431fae6f86
Alec Tower <alectower@gmail.com>
2015-06-09 14:37:04 -0400

    Add caching for search results

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -1,3 +1,9 @@
 module SchoolsHelper
+  def schools_cache_key
+    key = "schools/all/"
+    key << "#{@grade}/" if !@grade.blank?
+    key << "#{@loc}/" if !@loc.blank?
+    key << @schools.max_by { |s| s.timestamp.to_i }.timestamp
+  end
 end
 
