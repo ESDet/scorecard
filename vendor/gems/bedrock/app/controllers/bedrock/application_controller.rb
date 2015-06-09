2745bf4e60d207cfaa2832c1a5e88a8aac078729
Alec Tower <alectower@gmail.com>
2015-06-09 12:14:59 -0400

    Remove old maps implementation and unused code

--- vendor/gems/bedrock/app/controllers/bedrock/application_controller.rb
+++
@@ -1,4 +0,0 @@
-module Bedrock
-  class ApplicationController < ActionController::Base
-  end
-end
--- vendor/gems/bedrock/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  match 'geocode' => 'bedrock/geo#geocode'
-  match 'autocomplete/:address' => 'bedrock/geo#autocomplete'
-  match 'disable_gps' => 'bedrock/geo#disable_gps'
-  match 'enable_gps' => 'bedrock/geo#enable_gps'
-end
