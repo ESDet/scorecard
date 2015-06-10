68ab7a399faca54ef92cbd76f7f047734f9cbd9b
Alec Tower <alectower@gmail.com>
2015-06-10 07:35:24 -0400

    Remove Tip model and remove db schema

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,39 +3,14 @@ class HomeController < ApplicationController
 
   def index
     @school_names = []
-    @resources = Tip.find_by_name('resources')
   end
 
   def resources
-    @page = Tip.find_by_name('resources')
+    render 'shared/_resources'
   end
 
   def robots
     response = (Rails.env == 'production') ? '' : "User-Agent: *\nDisallow: /"
     render :text => response
-  end
-
-  def tips
-    @tips = Tip.all
-  end
-
-  def save_tips
-    tips = params[:tips]
-
-    tips.each do |t|
-      tip = Tip.find(t[:id])
-      tip.update_attributes(t)
-    end
-
-    flash[:notice] = 'Saved your changes.'
-    redirect_to '/tips'
-  end
-
-  protected
-
-  def password_protect
-    authenticate_or_request_with_http_basic do |user, password|
-      user == 'esd' && password == 'd3tr01t'
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 ESD::Application.routes.draw do
-
-  match 'search'            => 'home#search', :as => :search, :method => :post
+  root :to => "home#index"
   match 'resources'         => 'home#resources', :via => :get
   match 'schools'           => 'schools#index', :via => :post
   match 'schools/:id'       => 'schools#show', as: :school
   match 'compare(/*which)'  => 'schools#compare'
   match 'robots.txt'        => 'home#robots',    :as => :robots
-  match 'tips'              => 'home#tips', :via => :get
-  match 'tips'              => 'home#save_tips', :via => :post
-
-  root :to => "home#index"
-
 end
