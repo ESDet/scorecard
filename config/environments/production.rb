e992aaee5fdfb56f91ee87a8981bb49773452f72
Alec Tower <alectower@gmail.com>
2015-06-10 11:09:04 -0400

    Update production config variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,8 @@ ESD::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
-  # config.log_level = :debug
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-  config.serve_static_assets = false
-  # config.action_controller.asset_host = "http://assets.example.com"
-  # config.action_mailer.raise_delivery_errors = false
-  # config.threadsafe!
+  config.serve_static_files = false
+  config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
 
