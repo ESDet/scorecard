f69e330b2fe429c71c67b8e2968bea760274546e
Alec Tower <alectower@gmail.com>
2015-06-10 09:40:53 -0400

    Remove unused config

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ESD::Application.config.secret_token = 'd342a20b6e7c28e1ca311b81fd839d8c3c85a969ab6c171d81ff17cfbf2d06c369ec83db089215bea43cb130767702c6072fbed00be73fddce0cee5b01cc2e81'
