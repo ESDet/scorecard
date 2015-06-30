61ee9d0e4c2e4694cf9e3a79f687b88ad4b6cf6a
Alec Tower <alectower@gmail.com>
2015-06-30 14:19:49 -0400

    Add filters to search cache

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -3,6 +3,7 @@ module SchoolsHelper
     key = "schools/all/"
     key << "#{@grade}/" if !@grade.blank?
     key << "#{@loc}/" if !@loc.blank?
+    @filters.each { |f| key << "#{f}/" }
     key << @schools.max_by { |s| s.timestamp.to_i }.timestamp
   end
 end
