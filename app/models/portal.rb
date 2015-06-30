ca7aba0d7ea9b685c4044d1fdff204e5638ee1d0
Alec Tower <alectower@gmail.com>
2015-06-30 16:42:13 -0400

    Use test API for dev and test environments

--- app/models/portal.rb
+++ app/models/portal.rb
@@ -2,7 +2,11 @@ class Portal
   include HTTParty
   default_timeout 30
 
-  BASE = 'https://portal.excellentschoolsdetroit.org/api/1.0/'
+  BASE = if Rails.env.in?(['staging', 'production'])
+    'https://portal.excellentschoolsdetroit.org/api/1.0/'
+  else
+    'https://ec-scorecard-api-3mhev6qb5ihtc.us.platform.sh/api/1.0/'
+  end
 
   def list_vocabularies
     fetch 'taxonomy_vocabulary.json/'
@@ -17,8 +21,6 @@ class Portal
     result.first
   end
 
-  # Possible views: meap_2012*, esd_k8_2014, esd_hs_2014, act_2014...
-  # opts = { :limit => x, :offset => y }
   def get_dataset(view, bcode=nil, opts={})
     data = bcode.nil? ? {} : { 'filters[bcode]' => bcode }
     data.merge!(opts)
@@ -42,7 +44,6 @@ class Portal
     elsif method == :post
       headers = { 'Content-Type' => 'application/json' }
       response = Portal.post(url, { :body => data.to_json, :headers => headers }, verify: false)
-      #response, data = http.post(uri.path, data.to_json, headers)
     end
     JSON.parse(response.body) if response.body != ""
   end
