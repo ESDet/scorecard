474adf660df544f5732a9e026845e3957d36615a
Alec Tower <alectower@gmail.com>
2015-07-01 11:29:51 -0400

    Remove unused deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git pull
-lessc public/stylesheets/style.less > public/stylesheets/style.css
-lessc public/stylesheets/print.less > public/stylesheets/print.css
-touch tmp/restart.txt
-
-
