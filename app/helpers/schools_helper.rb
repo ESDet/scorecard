3e86b3719899df8237fed459397dcca426d9d955
Alec Tower <alectower@gmail.com>
2015-08-05 10:11:46 -0400

    Add michigan percentile to compare page

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -6,5 +6,17 @@ module SchoolsHelper
     @filters.sort.each { |f| key << "#{f}/" }
     key << @schools.max_by { |s| s.timestamp.to_i }.timestamp
   end
+
+  def percentile_suffix(percentile)
+    p = percentile.to_i
+    if p < 10 || p > 20
+      return "st" if percentile.end_with?("1")
+      return "nd" if percentile.end_with?("2")
+      return "rd" if percentile.end_with?("3")
+      "th"
+    else
+      "th"
+    end
+  end
 end
 
