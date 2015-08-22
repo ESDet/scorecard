354857e47b7ad8363cf9c2f502f0f179ad7f0d38
Alec Tower <alectower@gmail.com>
2015-08-22 10:24:16 -0400

    Update hide/show of schools on compare page

--- app/assets/javascripts/compare.js
+++ app/assets/javascripts/compare.js
@@ -31,32 +31,125 @@ var Compare = function(w) {
   };
 
   var _hideScroll = function(nextSchool, scrollEl) {
-    if (!nextSchool.hasClass('school')) {
-      scrollEl.addClass('hide');
+    if (nextSchool.hasClass('school')) {
+      scrollEl.removeClass('hide');
+      scrollEl.css('display', 'inline-block');
     } else {
-      $('.scroll > div').removeClass('hide');
+      scrollEl.addClass('hide');
+      scrollEl.css('display', 'none');
+    }
+  };
+
+  var _setHidden = function() {
+    var schools = $('.compare > .school');
+    var width = _w.width();
+    if (width > 1271) {
+      schools.removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '0');
+    } else if (width < 1272 && width > 801) {
+      $(schools[0]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '6%');
+      $(schools[1]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '0');
+      $(schools[2]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '0');
+      $(schools[3]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
+    } else if (width < 802 && width > 540) {
+      $(schools[0]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '2%');
+      $(schools[1]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '0');
+      $(schools[2]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
+      $(schools[3]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
+    } else if (width < 541) {
+      $(schools[0]).
+        removeClass('hide').
+        css('display', 'inline-block').
+        css('margin-left', '2%');
+      $(schools[1]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
+      $(schools[2]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
+      $(schools[3]).
+        addClass('hide').
+        css('display', 'none').
+        css('margin-left', '0');
     }
   };
 
   var _showSchool = function(arg) {
-    if (w.width() < 501)
-    var currentSchool = $('.compare > .school').
-      not('.hide').first();
+    var swapSchools = function(currentSchool, showSchool) {
+      if (showSchool.hasClass('school')) {
+        currentSchool.addClass('hide');
+        currentSchool.css('display', 'none');
+        showSchool.removeClass('hide');
+        showSchool.css('display', 'inline-block');
+        _updateNavName(_w.scrollTop(), _navPos);
+      }
+    };
 
+    var visibleSchools = function() {
+      return $('.compare > .school').not('.hide');
+    };
+
+    var currentSchools = visibleSchools();
     var showSchool, arrowEl;
-    if (arg == 'next') {
-      showSchool = currentSchool.next();
-      _hideScroll(showSchool.next(), $('.scroll > .right'));
-    } else if (arg == 'prev') {
-      showSchool = currentSchool.prev();
-      _hideScroll(showSchool.prev(), $('.scroll > .left'));
-    }
-
-    if (showSchool.hasClass('school')) {
-      currentSchool.addClass('hide');
-      showSchool.removeClass('hide');
-      showSchool.css('display', 'inline-block');
-      _updateNavName(_w.scrollTop(), _navPos);
+    var margin = currentSchools.first().css('margin-left');
+    if (currentSchools.length > 1) {
+      if (arg == 'next') {
+        showSchool = currentSchools.last().next();
+        if (showSchool.hasClass('school')) {
+          swapSchools(currentSchools.first(), showSchool);
+        }
+      } else if (arg == 'prev') {
+        showSchool = currentSchools.first().prev();
+        if (showSchool.hasClass('school')) {
+          swapSchools(currentSchools.last(), showSchool);
+        }
+      }
+      currentSchools = visibleSchools();
+      currentSchools.css('margin-left', '0');
+      currentSchools.first().css('margin-left', margin);
+      _hideScroll(currentSchools.first().prev(), $('.scroll > .left'));
+      _hideScroll(currentSchools.last().next(), $('.scroll > .right'));
+    } else {
+      var currentSchool = currentSchools.first();
+      if (arg == 'next') {
+        showSchool = currentSchool.next();
+      } else if (arg == 'prev') {
+        showSchool = currentSchool.prev();
+      }
+      swapSchools(currentSchool, showSchool);
+      currentSchools = visibleSchools();
+      currentSchools.css('margin-left', '0');
+      currentSchool = currentSchools.first();
+      currentSchool.css('margin-left', margin);
+      _hideScroll(currentSchool.prev(), $('.scroll > .left'));
+      _hideScroll(currentSchool.next(), $('.scroll > .right'));
     }
     return false;
   };
@@ -76,6 +169,7 @@ var Compare = function(w) {
     hideScroll: _hideScroll,
     hideSchools: _hideSchools,
     showSchool: _showSchool,
-    setStickyNav: _setStickyNav
+    setStickyNav: _setStickyNav,
+    setHidden: _setHidden
   }
 };
