e0b0701d714895190d0793f8689f5206948de5c8
Richard Peng <richard@richardpeng.com>
2016-03-29 09:50:02 -0400

    Set limit of schools to fetch from API to really high number to fetch all schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -7,7 +7,7 @@ class SchoolsController < ApplicationController
     @school_ids = school_ids.uniq[0..3].join(",") if school_ids
     @offset = params[:offset] || 0
     @ecs_offset = params[:ecs_offset] || 0
-    @limit = params[:limit] || 25
+    @limit = params[:limit] || 10000
 
     @grade = params[:grade]
     @filters = params[:filters] || []
@@ -24,7 +24,7 @@ class SchoolsController < ApplicationController
     license_types = []
     case @grade
     when "ec"
-      url = "ecs.json?offset=#{@ecs_offset}&flatten_fields=true" <<
+      url = "ecs.json?offset=#{@ecs_offset}&flatten_fields=true&limit=#{@limit}" <<
         "&includes=most_recent_ec_state_rating,ec_profile,esd_el_2015" <<
         "&sort_by_special=ec_total_pts" <<
         "&sort_order_special=DESC" <<
@@ -53,7 +53,7 @@ class SchoolsController < ApplicationController
     when "k8", "hs", "high"
       @grade = "hs" if @grade == "high"
 
-      url = "schools.json?offset=#{@offset}&flatten_fields=true" <<
+      url = "schools.json?offset=#{@offset}&flatten_fields=true&limit=#{@limit}" <<
         "&includes=school_profile,esd_#{@grade}_2016" <<
         "&sort_by_special=school_combined_total_pts" <<
         "&sort_order_special=DESC" <<
@@ -86,14 +86,14 @@ class SchoolsController < ApplicationController
         end
       end
     else
-      schools_url = "schools.json?offset=#{@offset}" <<
+      schools_url = "schools.json?offset=#{@offset}&limit=#{@limit}" <<
         "&flatten_fields=true" <<
         "&includes=school_profile,esd_k8_2016," <<
         "esd_hs_2016&filter[field_scorecard_display]=1" <<
         "&sort_by_special=school_combined_total_pts" <<
         "&sort_order_special=DESC"
 
-      ecs_url = "ecs.json?offset=#{@ecs_offset}&flatten_fields=true" <<
+      ecs_url = "ecs.json?offset=#{@ecs_offset}&limit=#{@limit}&flatten_fields=true" <<
         "&includes=most_recent_ec_state_rating,ec_profile,esd_el_2015" <<
         "&filter[field_scorecard_display]=1" <<
         "&sort_by_special=ec_total_pts" <<
